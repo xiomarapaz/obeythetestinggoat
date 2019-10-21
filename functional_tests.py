6ab2256c3d3d0335b36d4162f3f5d6c04fae44d6
xiomarapaz <xiomarapaz69@gmail.com>
2019-10-21 16:57:07 +0200

    finishing exercise 4

--- functional_tests.py
+++ functional_tests.py
@@ -8,8 +8,8 @@ class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+#	def tearDown(self):
+#		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
--- lists/tests.py
+++ lists/tests.py
@@ -5,11 +5,6 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
 
@@ -17,8 +12,8 @@ class HomePageTest(TestCase):
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do lists</title>', html)
 		self.assertTrue(html.strip().endswith('</html>'))
-		self.assertTemplateUsed(response, 'wrong.html')
-	
+#		self.assertTemplateUsed(response, 'wrong.html')
+
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
