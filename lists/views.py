ec0f130a1a860d64c54b2f673b67bae5a9f80515
xiomarapaz <xiomarapaz69@gmail.com>
2019-10-25 17:29:02 +0200

    finished until chapter 5.7

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 def home_page(request):
-#    if request.method == 'POST':
-    return render(request, 'home.html', {
-    'new_item_text': request.POST.get('item_text', ''), })
-#    return render (request, 'home.html')
+
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])  
+		return redirect('/')
+
+	return render(request, 'home.html')
