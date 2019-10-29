6c8d35cadabe875ccfcacedc60585c9a893ba18e
xiomarapaz <xiomarapaz69@gmail.com>
2019-10-29 16:12:34 +0100

    redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -13,7 +13,6 @@ class HomePageTest(TestCase):
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do lists</title>', html)
 		self.assertTrue(html.strip().endswith('</html>'))
-#		self.assertTemplateUsed(response, 'wrong.html')
 
 	def test_uses_home_template(self):
 		response = self.client.get('/')
@@ -21,11 +20,10 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		self.client.post('/', data={'item_text': 'A new list item'})
-	
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first() 
 		self.assertEqual(new_item.text, 'A new list item') 
-		
+	
 	def test_redirects_after_POST(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
@@ -34,6 +32,15 @@ class HomePageTest(TestCase):
 	def test_only_saves_items_when_necessary(self):
         	self.client.get('/')
         	self.assertEqual(Item.objects.count(), 0)
+
+	def test_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/')
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,8 @@ from lists.models import Item
 def home_page(request):
 
 	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])  
+		Item.objects.create(text=request.POST['item_text'])
 		return redirect('/')
 
-	return render(request, 'home.html')
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
